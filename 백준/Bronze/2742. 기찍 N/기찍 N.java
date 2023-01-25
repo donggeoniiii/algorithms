9bd8699114d301216f4944a6d11b56d0b6343579
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-01-26 08:51:51 +0900

    [Bronze IV] Title: 기찍 N, Time: 1488 ms, Memory: 35984 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int N = input.nextInt();
+		for (int i = N; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+}
