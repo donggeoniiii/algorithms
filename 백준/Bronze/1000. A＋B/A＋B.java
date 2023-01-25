c180cc6774c59b26f1fa0f152f070074977fd1b4
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-01-25 13:04:27 +0900

    [Bronze V] Title: A+B, Time: 112 ms, Memory: 12856 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        int A = input.nextInt();
+        int B = input.nextInt();
+        
+        System.out.println(A+B);
+    }
+}
