256253f3503701863c422aad189a320353fe8821
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-01-26 08:50:50 +0900

    [Bronze V] Title: N 찍기, Time: 1408 ms, Memory: 32484 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int N = input.nextInt();
+		for (int i = 0; i < N; i++) {
+			System.out.println(i+1);
+		}
+	}
+}
