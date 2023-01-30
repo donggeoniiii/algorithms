19fe95f0fe6530cba021568765609b09fae30d32
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-01-30 17:05:18 +0900

    Update 에라토스테네스의 체.java

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -1,40 +1,31 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        //n, k 입력 받기
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        primeNumber(n,k);
-    }
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 
-	//에라토스테네스의 체 알고리즘
-    static void primeNumber(int n, int k) {
-        int[] arr = new int[n+1];  ////n까지 저장할 수 있는 배열 선언
-
-        //배열 초기화화
-       for (int i = 2; i <= n; i++) {
-            arr[i] = i;
-        }
-        for (int i = 2; i <= n; i++) {
-            if (arr[i] == 0) continue; //이미 지운 수는 건너뛰기
-
-            //배수들 지우기
-            for (int j = i; j <= n; j+=i) {
-                if (arr[j] != 0) {
-                    arr[j] = 0;
-                    k--;
-                    //k번째로 지운 수 출력
-                    if (k == 0) {
-                        System.out.print(j);
-                        return;
-                    }
-                }
-            }
-        }
-    }
+		Stack<Integer> nums = new Stack<>();
+		int n = 2;
+		
+		while (nums.size() < K) {
+			for (int i = 1; i * n < N + 1; i++) {
+				if (nums.contains(i*n)) {
+					continue;
+				}
+				nums.add(i * n);
+				if (nums.size() == K) break;
+			}
+			n++;
+		}		
+		System.out.println(nums.pop());
+	}
 }
