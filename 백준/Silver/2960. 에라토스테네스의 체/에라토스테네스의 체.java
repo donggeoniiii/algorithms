e2c6cb063ba129dc30186fe4f5282350eaa336d5
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-01-30 16:49:43 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 80 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -1,28 +1,40 @@
-import java.util.Scanner;
 
+import java.io.*;
+import java.util.*;
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int K = sc.nextInt();
-		int cnt = 0;
-		int[] ar = new int[N+1];
-		
-		for(int i=0; i<=N; i++) {
-			ar[i] = i;
-		}
-		
-		ar[0] = ar[1] = 0; 
-		
-		for(int i=2; i<=N; i++) {
-			for(int j=1; i*j<=N; j++) {
-				if(ar[i*j] != 0) {  
-					cnt++; 
-					if(cnt == K) { System.out.println(ar[i*j]); }
-					ar[i*j] = 0;
-				}
-			}
-		}
-		
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        //n, k 입력 받기
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        primeNumber(n,k);
+    }
+
+	//에라토스테네스의 체 알고리즘
+    static void primeNumber(int n, int k) {
+        int[] arr = new int[n+1];  ////n까지 저장할 수 있는 배열 선언
+
+        //배열 초기화화
+       for (int i = 2; i <= n; i++) {
+            arr[i] = i;
+        }
+        for (int i = 2; i <= n; i++) {
+            if (arr[i] == 0) continue; //이미 지운 수는 건너뛰기
+
+            //배수들 지우기
+            for (int j = i; j <= n; j+=i) {
+                if (arr[j] != 0) {
+                    arr[j] = 0;
+                    k--;
+                    //k번째로 지운 수 출력
+                    if (k == 0) {
+                        System.out.print(j);
+                        return;
+                    }
+                }
+            }
+        }
+    }
 }
