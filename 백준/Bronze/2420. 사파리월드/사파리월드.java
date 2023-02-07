c63062b3d3f7a9a21f8d6522890eae804bed1c71
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-02-07 17:49:38 +0900

    [Bronze V] Title: 사파리월드, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		if (a - b >= 0) System.out.println(a-b);
+		else System.out.println(b-a);
+	}
+}
