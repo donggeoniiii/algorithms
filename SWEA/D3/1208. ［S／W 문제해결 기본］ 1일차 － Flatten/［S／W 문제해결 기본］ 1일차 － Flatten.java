fccbc30ff14dfcd3f68a1476bcfbb37bc297886d
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-02-14 17:10:36 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 165 ms, Memory: 21,504 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,11 +1,10 @@
 // Flatten
 
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Solution {
-	// 처음엔 전범위
+	// 선택정렬
 	static void selectionSort(int[] arr) {
 		int n = arr.length;
 		
@@ -27,7 +26,7 @@ public class Solution {
 		Scanner input = new Scanner(System.in);
 		StringTokenizer st;
 		
-		// 정렬
+		
 		for (int tc = 1; tc <= 10; tc++) {
 			int T = Integer.parseInt(input.nextLine());
 			
@@ -36,26 +35,31 @@ public class Solution {
 			
 			st = new StringTokenizer(input.nextLine());
 			
+			// 박스 배열 입력받기
 			for (int i = 0; i < 100; i++) {
 				boxes[i] = Integer.parseInt(st.nextToken());
 			}
 
+			// 정렬 시키기
 			selectionSort(boxes);
 	
+			// 지정된 덤프 횟수만큼 반복 진행
 			for (int i = 0; i < T; i++) {
 				int max = boxes[99];
 				int min = boxes[0];
 				// 최고점, 최저점 차이 비교
-				answer = max - min;
 				// 차이가 1 또는 0이면 덤프를 수행할 수 없으므로 종료, 그때의 높이차 반환
 				if (max - min <= 1) {
+					answer = max - min;
 					break;
 				}
 				// 끝나지 않으면 평탄화 작업 진행
 				else {
 					boxes[99] -= 1;
 					boxes[0] += 1;
+					
 					selectionSort(boxes);
+					
 					answer = boxes[99] - boxes[0];
 				}
 			}
