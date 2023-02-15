f4ea65a6166bdb3f5bb19d8e262c4f8accf7b9f3
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-02-16 00:00:13 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 228 ms, Memory: 34,280 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -9,12 +9,10 @@ public class Solution {
 		return col >= 0 && col < 100;
 	}
 	
-	
 	// 좌우에 사다리가 있는지 확인하는 메소드
 	static boolean ableToMove(int[][] arr, int r, int c) {
 		return inLadder(c) && arr[r][c] == 1;
 	}
-	
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -37,7 +35,7 @@ public class Solution {
 			int answer = 0;
 			
 			// 사다리타기 시작
-			for (int c = 1; c < 100; c++) {
+			for (int c = 0; c < 100; c++) {
 				// 좌표 탐색을 위한 변수 생성 
 				int row = 0, col = c;
 				
@@ -53,24 +51,24 @@ public class Solution {
 						while (ladder[row+1][col] != 1) {
 							col--;
 						}
-						// 이동하면
+						// 더 이상 이동할 수 없으면 밑으로 한 칸 가고 마무리
 						row++;
-						
-					// 오른쪽으로 이동할 수 있는지 확인
+					
+					// 오른쪽으로 이동할 수 있는지 확인(사다리 특성상 왼쪽, 오른쪽이 동시에 가능할 수는 없다)
 					} else if (ableToMove(ladder, row, col+1)) {
 						col++;
-						// 왼쪽으로 이동할 수 있다면 index를 벗어나지 않으면서 아래로 사다리가 날 때까지 계속 왼쪽으로 이동
+						// 오른쪽으로 이동할 수 있다면 index를 벗어나지 않으면서 아래로 사다리가 날 때까지 계속 왼쪽으로 이동
 						while (ladder[row+1][col] != 1) {
 							col++;
 						}
+						// 더 이상 이동할 수 없으면 밑으로 한 칸 가고 마무리
 						row++;
 					} 
 					
-					// 둘다 이동할 수 없다면 아래로 한 칸 이동
-					else {
-						row++;
-					}
+					// 가로로 이동할 수 없다면 면 아래로 한 칸 이동
+					else row++;
 
+					// 그렇게 도달한 마지막 행의 좌표값이 2(X)면 for문 탈출
 					if (row == 99 && ladder[row][col] == 2) {
 						answer = c;
 						break;
