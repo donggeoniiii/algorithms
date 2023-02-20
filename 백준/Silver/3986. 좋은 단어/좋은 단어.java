4e05f1a720b37d5bd8671726e7af7a22401f0e62
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-02-20 17:08:21 +0900

    [Silver IV] Title: 좋은 단어, Time: 684 ms, Memory: 59688 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,50 @@
+// 좋은 단어
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/*
+ * 선끼리 교차하지 않는다 == 괄호와 동일
+ * 만약 stack에 쌓는 과정에서 top이 )인데 (가 들어오면 나쁜 단어
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Stack<String> stack;
+		// 단어의 수
+		int N = Integer.parseInt(input.nextLine());
+		
+		// 착한 단어 카운트
+		int cnt = 0;
+		
+		for (int tc = 1; tc <= N; tc++) {
+			// 문자열 입력받아서 하나씩 쪼개기
+			String[] words = input.nextLine().split("");
+			stack = new Stack<>();
+			
+			for (String word : words) {
+				
+				// 비어있으면 추가
+				if (stack.isEmpty()) {
+					stack.push(word);
+				}
+				
+				else {
+					// 만약 top과 들어오는 단어가 같다면 pop
+					if (stack.peek().equals(word)) stack.pop();
+					// 아니면 그냥 추가
+					else stack.push(word);
+				}
+			}
+			
+			
+			// 만약 stack이 비면 착한 단어
+			if (stack.isEmpty()) cnt++;
+		}
+		
+		// 정답 출력
+		System.out.println(cnt);
+		
+	}
+}
