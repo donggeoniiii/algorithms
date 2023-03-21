3e99c15027490e9f4c48402003d7feefced76277
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-03-21 13:29:29 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,381 ms, Memory: 25,116 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Solution {
+public class Solution{
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
@@ -18,7 +18,7 @@ public class Solution {
 			int L = input.nextInt();
 			
 			// 민기의 맛에 대한 점수와 칼로리를 담는 배열
-			int[][] tasteNcal = new int[N][N];
+			int[][] tasteNcal = new int[N][2];
 			
 			// 담기, idx 0: 선호점수, idx 1: 칼로리
 			for (int i = 0; i < N; i++) {
@@ -31,7 +31,7 @@ public class Solution {
 			
 			
 			// 부분집합의 개수: 2^N == 1<<n
-			for (int idx = 0; idx < (1<<N); idx++) {
+			for (int pset = 0; pset < (1<<N); pset++) {
 
 				// 음식의 점수, 칼로리를 합산할 변수
 				int grade = 0;
@@ -41,7 +41,7 @@ public class Solution {
 				for (int food = 0; food < N; food++) {
 					
 					// 만약 포함된다면 해당 음식의 점수, 칼로리 합산
-					if ((idx & 1<<food) > 0) {
+					if ((pset & 1<<food) > 0) {
 						grade += tasteNcal[food][0];
 						cal += tasteNcal[food][1];
 					}
