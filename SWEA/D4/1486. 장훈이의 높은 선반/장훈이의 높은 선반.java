16fa8994e0a73c7f3137461855c202dd0eafbe47
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-03-24 09:58:08 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 134 ms, Memory: 20,900 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -1,10 +1,41 @@
-import java.util.Arrays;
-
 // 장훈이의 높은 선반
 
 import java.util.Scanner;
 
 public class Solution {
+	// 유효한 탑의 최소값
+	static int min;
+	
+	// 점원의 수
+	static int N;
+	
+	// 기준이 되는 선반의 높이
+	static int B;
+	
+	// 점원의 키 배열
+	static int[] height; 
+	
+	// 고려했는지 확인하는 배열
+	static boolean[] checked;
+	
+	static void powerset1(int cnt, int sum) {
+		// pruning: 만약 합이 이미 현재 최소값을 넘어섰으면 해당경우 스킵
+		if (sum > min) return;
+		
+		// base case: 모든 점원을 확인했다면 종료
+		if (cnt == N) {
+			// 만약 합이 B 이상이면서 최소값보다 낮으면 갱신
+			if (sum >= B && sum < min)
+				min = sum;
+			return;
+		}
+
+		powerset1(cnt + 1, sum + height[cnt]); // 해당 점원 추가하는 경우
+		powerset1(cnt + 1, sum); // 해당 점원 추가 안하는 경우
+
+	}
+
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		StringBuilder sb;
@@ -17,47 +48,28 @@ public class Solution {
 			sb.append("#").append(tc).append(" ");
 			
 			// 점원의 수
-			int N = input.nextInt();
+			N = input.nextInt();
 			
 			// 선반의 높이
-			int B = input.nextInt();
+			B = input.nextInt();
 			
-			// 점원의 키 배열
-			int[] height = new int[N];
+			// 배열 크기 설정
+			height = new int[N];
+			checked = new boolean[N];
 			
 			// 키 입력
 			for (int idx = 0; idx < N; idx++) {
 				height[idx] = input.nextInt(); 
 			}
-	
-			// 부분집합 구하기
-			int[] powerset = new int [1<<N];
 			
-			// i: 공집합을 포함한 모든 부분집합 (1<<N개)
-			for (int i = 0; i < (1<<N); i++) {
-				
-				// j: 각각의 부분집합에 들어갈 수 있는 원소들 (N개)
-				for (int j = 0; j < N; j++) {
-					
-					// 해당 원소가 들어가면 그 부분집합에 원소값 추가 
-					if ((i & (1<<j)) > 0) powerset[i] += height[j];  
-					
-				}
-			}
+			// 최소값 초기화
+			min = Integer.MAX_VALUE;
 			
-			// 배열에서 기준높이 B보다 높은 가장 낮은 값 찾기
-			int answer = Integer.MAX_VALUE;
-			
-			for (int i = 0; i < (1<<N); i++) {
-				
-				// 만약 해당 탑의 높이가 B이상이면서 현재까지 가장 낮다면 갱신
-				if (powerset[i] >= B && powerset[i] < answer) {
-					answer = powerset[i];
-				}
-			}
+			// 백트래킹 on
+			powerset1(0, 0);
 			
 			// 정답 추가
-			sb.append(answer - B);
+			sb.append(min - B);
 			
 			// 출력
 			System.out.println(sb.toString());
