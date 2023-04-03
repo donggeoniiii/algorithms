7d2aa6cfeb1067a52abdc121b369a3857bed57ed
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-04-03 14:42:54 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 217 ms, Memory: 42,128 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -1,27 +1,50 @@
-// 작업순서
+// 작업순서(stack)
 
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
-public class Solution {	
-	// 위상정렬을 위한 queue(index 저장)
-	static Queue<Integer> queue;
+public class Solution {
 	
-	// 그래프를 표현하는 인접리스트
+	// 진입차수를 보관하는 배열
+	static int[] inCnt;
+	
+	// 출력을 위한 stack
+	static Stack<Integer> topoResult;
+	
+	// 인접리스트
 	static ArrayList<Integer>[] adj;
 	
-	// 각 정점의 진입차수를 값으로 갖는 배열
-	static int[] inCnt;
+	// 이미 체크한 정점을 표시하는 배열
+	static boolean[] visited;
+	
+	// 시스템 스택을 이용한 위상정렬 메소드
+	static void topo(int idx) {
+		
+		// 해당 지점 방문체크
+		visited[idx] = true;
+		
+		// 이어지는 모든 정점들에 대해
+		for (int next : adj[idx]) {
+
+			// 아직 방문하지 않았다면 재귀 호출
+			if (!visited[next]) topo(next);
+			
+		}
+		
+		// 해당 지점 출력을 위해 stack에 추가
+		topoResult.add(idx);
+	}
 	
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
 		StringBuilder sb = new StringBuilder();
 		
+		// 테스트케이스 개수 10개로 고정
 		for (int tc = 1; tc <= 10; tc++) {
+			
+			// 출력형태 설정
 			sb.append("#").append(tc).append(" ");
 			
 			// 정점의 개수
@@ -30,54 +53,35 @@ public class Solution {
 			// 간선의 개수
 			int E = input.nextInt();
 			
-			// 배열 및 인접리스트 초기화
-			queue = new LinkedList<>();
+			// 자료구조 초기화
 			inCnt = new int[V+1];
+			topoResult = new Stack<>();
 			adj = new ArrayList[V+1];
-			for (int idx = 1; idx <= V; idx++) {
-				adj[idx] = new ArrayList<>();
-			}
+			visited = new boolean[V+1];
+			for (int idx = 1; idx <= V; idx++) adj[idx] = new ArrayList<>(); 
 			
-			// 간선 정보 입력 받기
+			// 정점간 인접 정보 저장
 			for (int edge = 1; edge <= E; edge++) {
 				int idx = input.nextInt();
 				int next = input.nextInt();
 				adj[idx].add(next);
+				
+				// 진입차수 계산
 				inCnt[next]++;
 			}
 			
-			// 위상정렬을 통해서 작업순서 찾기
-			// 1. 진입차수가 0인 지점은 queue에 넣기 
+			// 배열을 돌면서 진입차수가 0인 정점에서부터 DFS
 			for (int idx = 1; idx <= V; idx++) {
-				if (inCnt[idx] == 0) {
-					sb.append(idx).append(" "); // 출력형태에 추가
-					queue.offer(idx);
-				}
+				if (inCnt[idx] == 0) topo(idx);
 			}
 			
-			// 2. queue가 빌 때까지
-			while (!queue.isEmpty()) {
-				
-				// 2-1. 정점 좌표 가져오기
-				int cur = queue.poll();
-				
-				// 2-2. 해당 정점과 이어지는 좌표 진입차수 감소
-				for (int next : adj[cur]) {
-					
-					inCnt[next] -= 1;
-					
-					// 만약 0이 되면 queue에 추가
-					if (inCnt[next] == 0) {
-						sb.append(next).append(" "); // 출력형태에 추가
-						queue.offer(next);
-					}
-				}
+			// 정답 추가
+			while (!topoResult.isEmpty()) {
+				sb.append(topoResult.pop()).append(" ");
 			}
-			
-			// 출력형태 설정
 			sb.append("\n");
+			
 		}
-		
 		// 정답 출력
 		System.out.println(sb.toString());
 		
