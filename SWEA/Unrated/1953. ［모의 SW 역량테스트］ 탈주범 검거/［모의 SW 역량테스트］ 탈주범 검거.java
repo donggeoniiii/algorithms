45df46a4d2989b299f6c5fd8ce6e1dbfe79d2945
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-04-07 11:00:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 탈주범 검거, Time: 234 ms, Memory: 60,756 KB -BaekjoonHub

--- SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
+++ SWEA/Unrated/1953. ［모의 SW 역량테스트］ 탈주범 검거/［모의 SW 역량테스트］ 탈주범 검거.java
@@ -1,5 +1,7 @@
-// 도주범 검거
+// 도주범 검거(BFS)
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Solution {
@@ -16,7 +18,7 @@ public class Solution {
 	// 터널 지도
 	static int[][] map;
 	
-	// 방문 배열
+	// 방문 시간을 기록하는 배열
 	static int[][] visited;
 
 	// 터널 종류에 따른 델타 배열
@@ -24,65 +26,75 @@ public class Solution {
 	static int[][] dr = {{}, {-1,1,0,0}, {-1,1}, {0,0}, {-1,0}, {1,0}, {1,0}, {-1,0}};
 	static int[][] dc = {{}, {0,0,-1,1}, {0,0}, {-1,1}, {0,1}, {0,1}, {0,-1}, {0,-1}};
 	
-	// 터널의 연결 방향 별 이동 가능한 경우를 저장한 배열
-	// 순서대로 상 / 하 / 좌  / 우의 경우
-	static int[][] connected = {{1,2,5,6}, {1,2,4,7}, {1,3,4,5}, {1,3,6,7}};
-	
-	// DFS 메소드
-	static void DFS(int cr, int cc, int cnt) {
+	// BFS 메소드
+	static void BFS(int r, int c) {
 		
-		// base case: L번의 이동을 무사히(?) 마쳤으면 종료
-		if (cnt == L) 
-			return;
+		// queue 선언
+		Queue<Integer> queue = new LinkedList<>();
 		
-		// recursive case: 터널을 따라 이동
+		// 시작점 입력
+		queue.offer(r);
+		queue.offer(c);
 		
-		// 터널의 종류 
-		int ctrl = map[cr][cc];
-		
-		// 현재 터널의 중류에 따라 갈 수 있는 방향만 탐색
-		for (int dt = 0; dt < dr[ctrl].length; dt++) {
+		// 더 탐색할 지점이 없을 때까지
+		while (!queue.isEmpty()) {
 			
-			// 새로운 터널 좌표
-			int nr = cr + dr[ctrl][dt];
-			int nc = cc + dc[ctrl][dt];
+			// 탐색지점 좌표
+			int cr = queue.poll();
+			int cc = queue.poll();
+			
+			// 터널의 종류 
+			int ctrl = map[cr][cc];
+			
+			// 현재 터널의 중류에 따라 갈 수 있는 방향만 탐색
+			for (int dt = 0; dt < dr[ctrl].length; dt++) {
+				
+				// 새로운 터널 좌표
+				int nr = cr + dr[ctrl][dt];
+				int nc = cc + dc[ctrl][dt];
 
-			// index를 벗어나면 제외
-			if (nr >= N || nc >= M || nr < 0 || nc < 0)
-				continue;
-			
-			// 이어갈 수 없으면 제외
-			if (map[nr][nc] <= 0)
-				continue;
+				// index를 벗어나면 제외
+				if (nr >= N || nc >= M || nr < 0 || nc < 0)
+					continue;
+				
+				// 이어갈 수 없으면 제외
+				if (map[nr][nc] <= 0)
+					continue;
 
-			// 인접한 터널의 종류
-			int nctrl = map[nr][nc];
-			
-			// 현재 방향에서 인접한 터널이 현재 터널과 연결 가능한지 확인
-			// 두 터널이 서로 이어지는지 확인하는 변수
-			boolean impossible = true;
+				// 인접한 터널의 종류
+				int nctrl = map[nr][nc];
+				
+				// 현재 방향에서 인접한 터널이 현재 터널과 연결 가능한지 확인
+				// 두 터널이 서로 이어지는지 확인하는 변수
+				boolean impossible = true;
 
-			for (int ndt = 0; ndt < dr[nctrl].length; ndt++) {
-				// 만약 인접한 터널이 현재 터널과 상하나 좌우로 이어진다면
-				if ((dr[ctrl][dt] != 0 && dr[ctrl][dt] == -dr[nctrl][ndt]) || (dc[ctrl][dt] != 0 && dc[ctrl][dt] == -dc[nctrl][ndt]))
-					impossible = false; // 이동 가능 표시
-			}
-			
-			// 만약 이어지지 않아서 이동이 안되면 스킵
-			if (impossible) continue;
-			
-			// 방문한 곳인데 더 빨리 지나가는 방법이 있는 경우 
-			if(visited[nr][nc] != 0 && visited[nr][nc] <= visited[cr][cc] + 1) 
-				continue;
-			
-			// 방문시간 갱신
-			visited[nr][nc] = visited[cr][cc] + 1;
-			
-			// 다음 좌표로 이동
-			DFS(nr, nc, cnt+1);
-		}	
-		
-			
+				for (int ndt = 0; ndt < dr[nctrl].length; ndt++) {
+					// 만약 인접한 터널이 현재 터널과 상하나 좌우로 이어진다면
+					if ((dr[ctrl][dt] != 0 && dr[ctrl][dt] == -dr[nctrl][ndt]) || (dc[ctrl][dt] != 0 && dc[ctrl][dt] == -dc[nctrl][ndt]))
+						impossible = false; // 이동 가능 표시
+				}
+				
+				// 만약 이어지지 않아서 이동이 안되면 제외
+				if (impossible) 
+					continue;
+				
+				// 방문했거나 시간이 지나면 제외
+				if (visited[nr][nc] > 0)
+					continue;
+
+				// L번 안에 방문했으면 새로운 좌표를 찾았으니까 방문 표시
+				if (visited[cr][cc] + 1 <= L) {
+					visited[nr][nc] = visited[cr][cc] + 1;
+					
+					// 더 갈 수 있으면 새로운 탐색 좌표 추가
+					if (visited[nr][nc] < L) {
+						queue.offer(nr);
+						queue.offer(nc);
+					}
+				}
+
+			}	
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -123,8 +135,8 @@ public class Solution {
 			// 시작점 방문표시
 			visited[hr][hc] = 1;
 			
-			// DFS ON
-			DFS(hr, hc, 1);
+			// BFS ON
+			BFS(hr, hc);
 			
 			// 방문한 지역의 수 세기
 			int hidePoints = 0;
