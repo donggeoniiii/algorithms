c2599c84f94df6675914282a0e1841f3a3e95e8f
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-04-10 15:57:27 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,381 ms, Memory: 25,116 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,66 +1,77 @@
-// 햄버거 다이어트
+// 햄버거 다이어트(re)
 
 import java.util.Scanner;
 
-public class Solution{
+public class Solution {
+	// 칼로리 제한
+	static int L;
+	
+	// 주어지는 재료의 개수
+	static int N;
+	
+	// 재료별 점수, 칼로리 배열
+	static int[] points;
+	static int[] calories;
+	
+	// 최댓값
+	static int maxTaste;
+	
+	// 백트래킹 알고리즘
+	static void burger(int cnt, int cal, int cur) {
+		
+		// pruning: 칼로리 제한 넘어서면 종료
+		if (cal > L) 
+			return;
+		
+		// base case: N개의 재료 다 따져보면 종료
+		if (cnt == N) {
+			
+			maxTaste = Math.max(maxTaste, cur);
+			
+			return;
+		}
+		
+		// recursive case
+		// 안 선택
+		burger(cnt+1, cal, cur);
+			
+		// 선택
+		burger(cnt+1, cal + calories[cnt], cur + points[cnt]);
+		
+	}
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
-
+		
 		// 테스트케이스 개수
 		int T = input.nextInt();
 		
 		for (int tc = 1; tc <= T; tc++) {
-			sb.append("#").append(tc).append(" ");
 			
-			// 재료의 수 N, 제한 칼로리 L
-			int N = input.nextInt();
-			int L = input.nextInt();
-			
-			// 민기의 맛에 대한 점수와 칼로리를 담는 배열
-			int[][] tasteNcal = new int[N][2];
-			
-			// 담기, idx 0: 선호점수, idx 1: 칼로리
-			for (int i = 0; i < N; i++) {
-				tasteNcal[i][0] = input.nextInt();
-				tasteNcal[i][1] = input.nextInt();
-			}
-			
-			// 점수의 최댓값
-			int maxGrade = Integer.MIN_VALUE;
-			
-			
-			// 부분집합의 개수: 2^N == 1<<n
-			for (int pset = 0; pset < (1<<N); pset++) {
+			// 음식 개수, 제한 칼로리
+			N = input.nextInt();
+			L = input.nextInt();
 
-				// 음식의 점수, 칼로리를 합산할 변수
-				int grade = 0;
-				int cal = 0;
-				
-				// 각 부분집합 별로 해당 음식이 포함되는지 확인
-				for (int food = 0; food < N; food++) {
-					
-					// 만약 포함된다면 해당 음식의 점수, 칼로리 합산
-					if ((pset & 1<<food) > 0) {
-						grade += tasteNcal[food][0];
-						cal += tasteNcal[food][1];
-					}
-				}
-				
-				// 만약 칼로리 합이 제한 칼로리를 넘어가면 해당 경우 스킵
-				if (cal > L) continue;
-				
-				// 최댓값 갱신여부 확인
-				if (grade > maxGrade) maxGrade = grade;
+			// 배열 정보 입력
+			points = new int[N];
+			calories = new int[N];
+			for (int idx = 0; idx < N; idx++) {
+				points[idx] = input.nextInt();
+				calories[idx] = input.nextInt(); 
 			}
+			
+			// 제일 맛있는 조합 찾기
+			maxTaste = -1;
+			burger(0, 0, 0);
 			
 			// 정답 추가
-			sb.append(maxGrade).append("\n");
-			
+			sb.append("#").append(tc).append(" ").append(maxTaste).append("\n");
 		}
 		
-		// 출력
+		// 정답 출력
 		System.out.println(sb.toString());
 		
+		input.close();
 	}
 }
