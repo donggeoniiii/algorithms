c09abb13363fbb83d914dcca08f04ada03dfaf25
donggeoniiii <120314184+donggeoniiii@users.noreply.github.com>
2023-06-18 04:46:07 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1300 ms, Memory: 299132 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,5 +1,3 @@
-// 최소 스패닝 트리
-
 import java.util.*;
 
 public class Main {
@@ -7,46 +5,19 @@ public class Main {
     // 정점 N개, 간선 M개
     static int N, M;
 
-    // 연결리스트
-    static List<Integer>[] adj;
+    // 대표값이 누구인지 기억하는 배열
+    static int[] parent;
 
-    // bfs를 위한 queue
-    static Queue<Integer> queue;
+    // 대표값 갱신 메소드
+    static int findParent(int x){
+        // 만약 자기자신이 그룹의 대표값이면 그대로 자기 출력
+        if (parent[x] == x)
+            return x;
 
-    // bfs를 위한 방문배열
-    static boolean[] visited;
-
-    // bfs 알고리즘
-    static void bfs(int src){
-
-        // 시작점 추가
-        queue.offer(src);
-
-        // 시작점 방문 체크
-        visited[src] = true;
-
-        // 더 탐색할 지점이 없을 때까지
-        while (!queue.isEmpty()){
-
-            // 탐색할 다음 지점 꺼내오기
-            int cur = queue.poll();
-
-            // 연결된 모든 간선으로 넘어가기
-            for (int next : adj[cur]) {
-
-                // 만약 이미 방문한 곳이면 제외
-                if (visited[next])
-                    continue;
-
-                // 처음 가는 곳이면 다음 탐색에 추가
-                queue.offer(next);
-
-                // 방문 체크
-                visited[next] = true;
-            }
-        }
+        // 아니면 대표값 찾으러 그 위로 이동
+        else
+            return parent[x] = findParent(parent[x]);
     }
-
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -55,44 +26,34 @@ public class Main {
         N = input.nextInt();
         M = input.nextInt();
 
-        // 연결리스트 만들기
-        adj = new LinkedList[N+1];
+        // 대표값 저장 배열 만들기
+        parent = new int[N+1];
 
-        for (int i = 0; i <= N; i++){
-            adj[i] = new LinkedList<>();
+        // 처음엔 자기 자신으로 초기화
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
         }
 
         // 간선 정보 입력
         for (int edge = 1; edge <= M; edge++) {
             int src = input.nextInt();
             int dest = input.nextInt();
-            
-            // 무방향 그래프이므로 양쪽에 입력
-            adj[src].add(dest);
-            adj[dest].add(src);
+
+            // 둘의 대표값이 같지 않으면 하나로 합침(union)
+            if (findParent(src) != findParent(dest))
+                // 얘(parent[dest])를 대표값으로 가지는 모든 애들이 대표값이 바뀌어야 하니까
+                parent[findParent(dest)] = findParent(src);
         }
 
-        // BFS를 위해 queue, 방문배열 만들기
-        queue = new LinkedList<>();
-        visited = new boolean[N+1];
+        // 서로 다른 대표값의 개수를 구하기 위해 hashset 사용
+        HashSet<Integer> set = new HashSet<>();
 
-        // 연결요소의 개수는 곧 bfs로 한번에 묶이는 수와 같으므로 bfs 카운트 생성
-        int bfsCnt = 0;
-
-        // 전체 정점을 체크하면서
-        for (int node = 1; node <= N; node++){
-
-            // 아직 체크하지 않았으면 새로 bfs on
-            if (!visited[node]){
-                bfs(node);
-
-                // bfs 카운트 == 연결요소의 개수
-                bfsCnt++;
-            }
+        // 대표값 넣기
+        for (int i = 1; i <= N; i++){
+            set.add(findParent(i));
         }
 
         // 정답 출력
-        System.out.println(bfsCnt);
-
+        System.out.println(set.size());
     }
 }
