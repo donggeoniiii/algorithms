580091ea3eceac77113f1c5ce3d79fde0fb0b0a5
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2023-07-19 10:34:16 +0900

    [Gold IV] Title: 오큰수, Time: 1040 ms, Memory: 157028 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -3,8 +3,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -27,14 +26,17 @@ public class Main {
 		// 숫자 개수 N
 		int N = Integer.parseInt(br.readLine());
 
+		// 오큰수 저장 배열
+		int[] rightBigNumList = new int[N + 1];
+
+		// 안 빠지는 애들은 -1로 출력할 거니까 -1로 초기화
+		Arrays.fill(rightBigNumList, -1);
+
 		// 입력받은 숫자모음
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		// 숫자를 저장할 stack
 		Stack<Num> stack = new Stack<>();
-
-		// 출력을 위한 priorty Queue(최신기술..)
-		PriorityQueue<Num> pqueue = new PriorityQueue<>(Comparator.comparingInt(o -> o.idx));
 
 		// 모든 숫자 입력
 		for (int i = 1; i <= N; i++) {
@@ -43,22 +45,18 @@ public class Main {
 
 			// stack의 top과 비교
 			while (!stack.isEmpty() && stack.peek().value < next) {
-				// 출력을 위해 pqueue에 추가
-				pqueue.offer(new Num(stack.pop().idx, next));
+				// 출력을 위해 list에 추가
+				rightBigNumList[stack.pop().idx] = next;
 			}
 
 			// stack에 새로운 수 입력
 			stack.push(new Num(i, next));
 		}
 
-		// 남은 애들은 어차피 안빠지니까 다 -1로 처리
-		while (!stack.isEmpty()) {
-			pqueue.offer(new Num(stack.pop().idx, -1));
-		}
-
 		// 정답 출력
-		while (!pqueue.isEmpty())
-			sb.append(pqueue.poll().value).append(" ");
+		for (int i = 1; i <= N; i++) {
+			sb.append(rightBigNumList[i]).append(" ");
+		}
 		System.out.println(sb);
 	}
 }
