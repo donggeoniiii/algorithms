c4aacddb55865bc519a238e0790aa6287a7164aa
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2023-08-14 12:28:18 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -3,22 +3,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		// 수 입력받기
-		char[] input = br.readLine().toCharArray();
-
+		String input = br.readLine();
+		ArrayList<Character> sortedList = new ArrayList<>();
+		for (int i = 0; i < input.length(); i++) {
+			sortedList.add(input.charAt(i));
+		}
 		// 정렬
-		Arrays.sort(input);
+		Collections.sort(sortedList);
 
 		// 출력
 		StringBuilder sb = new StringBuilder();
-		for (int i = input.length - 1; i >= 0; i--) {
-			sb.append(input[i]);
+		for (int i = sortedList.size() - 1; i >= 0; i--) {
+			sb.append(sortedList.get(i));
 		}
 		System.out.println(sb);
 	}
