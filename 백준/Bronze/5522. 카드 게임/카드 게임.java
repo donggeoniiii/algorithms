7d1b714fd404d3846e859a93b700dfd0f9ec9d54
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2023-10-06 03:05:33 +0900

    [Bronze V] Title: 카드 게임, Time: 120 ms, Memory: 14216 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,19 @@
+// 카드게임
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 더하기
+		int total = 0;
+		for (int i = 0; i < 5; i++) {
+			total += Integer.parseInt(br.readLine());
+		}
+
+		System.out.println(total);
+	}
+}
