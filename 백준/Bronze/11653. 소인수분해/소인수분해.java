2ccacfd49a11cb9c64796e7c5970f313a54d1c1a
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2024-02-09 15:15:10 +0900

    [Bronze I] Title: 소인수분해, Time: 168 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,18 +1,36 @@
+// 소인수분해
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num = 2;
-		int N = Integer.parseInt(br.readLine());
-		while (N != 1) {
-			if (N % num == 0) {
-				N /= num;
-				System.out.println(num);
-			}
-			else num++;
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        // 소인수 모음
+        StringBuilder sb = new StringBuilder();
+
+        // n이 1이면 출력 안함
+        if (n != 1) {
+            // 2부터 하나씩 나눠지는지 확인
+            int div = 2;
+            while (n > 1) {
+                // 나눠지면 계속 나누기
+                if (n % div == 0) {
+                    n /= div;
+                    sb.append(div).append("\n");
+                }
+                // 안 나눠지면 나누는 수 +1
+                else {
+                    div++;
+                }
+            }
+            // 출력
+            System.out.println(sb);
+        }
+
+    }
 }
