3860582baeeafbed9250f3b80e8eddd973ac0948
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2024-03-10 21:22:06 +0900

    [Silver II] Title: N번째 큰 수, Time: 952 ms, Memory: 244412 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,28 +1,32 @@
+// n번째 큰 수
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.Hashtable;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-		
-		int N = Integer.parseInt(bf.readLine());
-		StringTokenizer st;
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(bf.readLine());
-			for(int j=0; j<N; j++) {
-				pq.offer(Integer.parseInt(st.nextToken()));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// x번째 큰 수
+		int n = Integer.parseInt(br.readLine());
+
+		// 정렬을 위한 우선순위 큐
+		PriorityQueue<Integer> nums = new PriorityQueue<>();
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				nums.add(Integer.parseInt(st.nextToken()));
+
+				// 만약 크기가 n을 초과하면 맨 앞에 값은 쳐내기
+				if (nums.size() > n) {
+					nums.poll();
+				}
 			}
 		}
-		
-		for(int i=1; i<N; i++) {
-			pq.poll();
-		}
-		System.out.println(pq.poll());
+
+		// 출력
+		System.out.println(nums.peek());
 	}
 }
