2a325815f80d47b986031efb58c1d11319f5fde3
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2024-08-02 11:36:06 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 14.34 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
@@ -12,26 +12,26 @@ class Solution {
         // 시작시간부터 종료시간까지 초단위 진행
         while (t1 < t2) {
             // 현재 위치 각도로 표시
-            double chdx = (t1 / 120D) % 360;
-            double cmdx = (t1 / 10D) % 360;
-            double csdx = (t1 * 6) % 360;
+            double curHourIdx = (t1 / 120D) % 360;
+            double curMinuteIdx = (t1 / 10D) % 360;
+            double curSecondIdx = (t1 * 6) % 360;
             
             // 다음 위치 각도로 표시
-            double nhdx = ((t1 + 1) / 120D) % 360;
-            double nmdx = ((t1 + 1) / 10D) % 360;
-            double nsdx = ((t1 + 1) * 6) % 360;
+            double nextHourIdx = ((t1 + 1) / 120D) % 360;
+            double nextMinuteIdx = ((t1 + 1) / 10D) % 360;
+            double nextSecondIdx = ((t1 + 1) * 6) % 360;
             
             // 다시 정각이 되어 0도가 되는 경우 360도로 계산
-            if (nhdx == 0) nhdx = 360;
-            if (nmdx == 0) nmdx = 360;
-            if (nsdx == 0) nsdx = 360;
+            if (nextHourIdx == 0) nextHourIdx = 360;
+            if (nextMinuteIdx == 0) nextMinuteIdx = 360;
+            if (nextSecondIdx == 0) nextSecondIdx = 360;
             
             // 만약 초침이 시, 분침을 지나면 +1
-            if (csdx < chdx && nsdx >= nhdx) answer++;
-            if (csdx < cmdx && nsdx >= nmdx) answer++;
+            if (curSecondIdx < curHourIdx && nextSecondIdx >= nextHourIdx) answer++;
+            if (curSecondIdx < curMinuteIdx && nextSecondIdx >= nextMinuteIdx) answer++;
             
             // 그중 시, 분침이 겹치는 경우는 카운트 중복 하나 제거
-            if (nhdx == nmdx) answer--;;
+            if (nextHourIdx == nextMinuteIdx) answer--;;
             
             // 1초 경과
             t1 += 1;
