a438f1cf736ea26b9c1740d09e8d7e16a37b18d2
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2025-03-06 16:25:01 +0900

    [level 1] Title: 모의고사, Time: 1.76 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -4,105 +4,38 @@ class Solution {
     public int[] solution(int[] answers) {
         int[] answer = {};
         
-        int[][] scores = new int[3][2];
+        int[][] patterns = {
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
+        };
+        int[] scores = new int[3];
+        for (int q = 0; q < answers.length; q++) {
+            for (int p = 0; p < patterns.length; p++) {
+                if (answers[q] == patterns[p][q % patterns[p].length]) scores[p]++;
+            }     
+        }
         
-        scores[0][0] = 1;
-        scores[0][1] = solve1(answers);
-        scores[1][0] = 2;
-        scores[1][1] = solve2(answers);
-        scores[2][0] = 3;
-        scores[2][1] = solve3(answers);
-        
-        // 점수 기준으로 정렬
-        Arrays.sort(scores, Comparator.comparingInt((int[] o) -> o[1]));
-        
-        int maxScore = scores[2][1];
+        int maxScore = -1;
         int maxCount = 0;
-        
-        for (int i = 0; i < 3; i++) {
-            if (scores[i][1] == maxScore) maxCount++;
+        for (int p = 0; p < patterns.length; p++) {
+            if (scores[p] >= maxScore) {
+                if (scores[p] > maxScore) {
+                    maxScore = scores[p];
+                    maxCount = 1;
+                } else {
+                    maxCount++;
+                }
+            }
         }
         
         answer = new int[maxCount];
         
         int idx = 0;
-        for (int i = 0; i < 3; i++) {
-            if (scores[i][1] == maxScore) {         
-                answer[idx++] = scores[i][0];   
-            }
+        for (int p = 0; p < patterns.length; p++) {
+            if (scores[p] == maxScore) answer[idx++] = p+1;
         }
-        
+    
         return answer;
-    }
-    
-    static int solve1(int[] answers) {
-        int score = 0;
-        
-        for (int q = 1; q <= answers.length; q++) {
-            int seq = q % 5;
-            int qIdx = q-1;
-            
-            if (seq == 0) {
-                if (answers[qIdx] == 5) score++;
-            } else { 
-                if (answers[qIdx] == seq) score++;
-            }
-        }
-        
-        return score;
-    }
-    
-    static int solve2(int[] answers) {
-        int score = 0;
-        
-        for (int q = 1; q <= answers.length; q++) {
-            int seq = q % 8;
-            int qIdx = q-1;
-                
-            switch (seq) {
-                case 0:
-                    if (answers[qIdx] == 5) score++;
-                    break;
-                case 2:
-                    if (answers[qIdx] == 1) score++;
-                    break;
-                case 4:
-                    if (answers[qIdx] == 3) score++;
-                    break;
-                case 6:
-                    if (answers[qIdx] == 4) score++;
-                    break;
-                default:
-                    if (answers[qIdx] == 2) score++;
-                    break;
-            }
-        }
-        
-        return score;
-    }
-    
-    static int solve3(int[] answers) {
-        int score = 0;
-        
-        for (int q = 1; q <= answers.length; q++) {
-            int seq = q % 10;
-            int qIdx = q-1;
-            
-            if (seq == 0) {
-                if (answers[qIdx] == 5) score++;
-            } else if (seq <= 2) {
-                if (answers[qIdx] == 3) score++;
-            } else if (seq <= 4) {
-                if (answers[qIdx] == 1) score++;
-            } else if (seq <= 6) {
-                if (answers[qIdx] == 2) score++;
-            } else if (seq <= 8) {
-                if (answers[qIdx] == 4) score++;
-            } else { // seq == 9
-                if (answers[qIdx] == 5) score++;
-            }
-        }
-        
-        return score;
     }
 }
