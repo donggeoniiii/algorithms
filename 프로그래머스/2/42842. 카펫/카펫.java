08109d88828b5f15c50a3dc0659b189689d0648e
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2025-03-07 15:25:20 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,26 +1,19 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
+        int[] answer = {};
         
         int total = brown + yellow;
         
-        return solve(total, brown, 1);
-    }
-    
-    static int[] solve(int total, int brown, int div) {
-        if (total % div != 0) {
-            return solve(total, brown, div+1);
+        for (int col = 1; col <= total / col; col++) {
+            if (total % col != 0) continue;
+            
+            int row = total / col;
+            
+            if (2 * (row + col) - 4 == brown) {
+                answer = new int[]{row, col};
+            }
         }
         
-        int row = total / div;
-        int col = div;
-        
-        if (row < col) return new int[] {-1, -1};
-        
-        if ((2 * (row + col) - 4) == brown) {
-            return new int[]{row, col};
-        } else {
-            return solve(total, brown, div+1);   
-        }
-        
+        return answer;
     }
 }
