9800c250dd4fefd7e16abb2126a065ab4c0db06c
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2025-09-25 23:20:09 +0900

    [level 2] Title: 타겟 넘버, Time: 19.27 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,30 +1,23 @@
 class Solution {
-    static int answer, n;
+    static int answer;
+    static int[] dt = {-1, 1};
+    
     public int solution(int[] numbers, int target) {
-        answer = 0;
-        n = numbers.length;
-        
-        int[] selected = new int[n];
-        findTargetCase(numbers, target, selected, 0);
+        selectSign(numbers, target, 0, 0);
         
         return answer;
     }
     
-    static void findTargetCase(int[] numbers, int target, int[] selected, int count) {
-        if (count == n) {
-            int total = 0;
-            for (int i = 0; i < n; i++) {
-                total += selected[i] * numbers[i];
-            }
-            
-            if (total == target) answer++;
+    void selectSign(int[] numbers, int target, int cur, int idx) {
+        if (idx >= numbers.length) {
+            if (cur == target) answer++;
             return;
         }
         
-        // 더하고 빼는 두 경우로 나눠서 진행
-        selected[count] = 1;
-        findTargetCase(numbers, target, selected, count+1);
-        selected[count] = -1;
-        findTargetCase(numbers, target, selected, count+1);
+        for (int i = 0; i < 2; i++) {
+            int next = numbers[idx] * dt[i];
+            
+            selectSign(numbers, target, cur + next, idx+1);
+        }
     }
 }
