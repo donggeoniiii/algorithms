102187b3c5f24a32c43c879a6cd9d9be27e9749e
박동건 Dong-Geon Park <120314184+donggeoniiii@users.noreply.github.com>
2025-09-26 12:53:53 +0900

    [level 3] Title: 여행경로, Time: 89.02 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,50 +1,31 @@
 import java.util.*;
 
 class Solution {
-    static String[] answer;
-    static boolean[] isUsed;
-    
     public String[] solution(String[][] tickets) {
-        answer = new String[1];
-        isUsed = new boolean[tickets.length];
+        List<String> answer = new ArrayList<>();
+        boolean[] visited = new boolean[tickets.length];
+        findPossibleRoutes(0, "ICN", "ICN", tickets, answer, visited);
         
-		List<String> route = new ArrayList<>();
-		route.add("ICN");
-		findPossibleRoute(tickets, 0, route.get(0), isUsed, route);
-
-        return answer;
+        answer.sort(null);
+        return answer.get(0).split(" ");
     }
     
-    void findPossibleRoute(String[][] tickets, int count, String cur, boolean[] isUsed, List<String> route) {
+    void findPossibleRoutes(int count, String src, String route,
+                            String[][] tickets, List<String> answer, boolean[] visited) {
         if (count >= tickets.length) {
-            String[] curRoute = route.toArray(String[]::new);
-            
-            if (answer.length == 1) {
-                answer = curRoute;
-                return;
-            }
-            
-            // 만약 이번 루트가 사전순으로 먼저 오면 갱신
-            if (Arrays.compare(answer, curRoute) >= 0) answer = curRoute;
+            answer.add(route);
+            return;
         }
         
         for (int i = 0; i < tickets.length; i++) {
-            // 이미 탑승한 동선이면 스킵
-            if (isUsed[i]) continue;
+            if (visited[i]) continue;
             
-            String src = tickets[i][0];
-            String dest = tickets[i][1];
+            if (!src.equals(tickets[i][0])) continue;
             
-            // 출발지점이 여기가 아니면 스킵
-            if (!src.equals(cur)) continue;
-            
-            route.add(dest);
-            isUsed[i] = true;
-            findPossibleRoute(tickets, count+1, dest, isUsed, route);
-            
-            // 다른 루트 탐색을 위해 초기화
-            route.remove(route.size() - 1);
-            isUsed[i] = false;
+            visited[i] = true;
+            findPossibleRoutes(count + 1, tickets[i][1], route + " " + tickets[i][1],
+                             tickets, answer, visited);
+            visited[i] = false;
         }
     }
 }
